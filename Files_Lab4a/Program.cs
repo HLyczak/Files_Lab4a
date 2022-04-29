44afde9f7a6f3ff688d971b2cc1d3ea40839276a
Honorata92 <h.sajdak1@o2.pl>
2022-04-29 21:58:29 +0200

    Dodaj pliki projektów.

---
+++ Files_Lab4a/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Files_Lab4a
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
